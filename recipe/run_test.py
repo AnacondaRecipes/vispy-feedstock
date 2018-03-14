e4232784b783ce815e9059b43f95d7f2359bd4e6
Nehal J Wani <nehaljw.kkd1@gmail.com>
2018-03-14 00:23:53 -0500

    Add some tests from old feedstock

---
+++ recipe/run_test.py
@@ -0,0 +1,14 @@
+import vispy.io
+import vispy.app
+import vispy.gloo
+
+import vispy.glsl
+import vispy.glsl.antialias
+import vispy.glsl.arrows
+import vispy.glsl.collections
+import vispy.glsl.colormaps
+import vispy.glsl.markers
+import vispy.glsl.math
+import vispy.glsl.misc
+import vispy.glsl.transforms
+
